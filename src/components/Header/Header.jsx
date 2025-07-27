48c6dcd765b52255435b0c5dae752bba7eaa16b4
junaid-shaikh2 <junaid.shaikh.kn@gmail.com>
2025-07-27 20:21:14 +0500

    Changed pics settings

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -22,7 +22,7 @@ export default function Header() {
                     <Link to="/" className="flex items-center">
                         <img
                             src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-                            className="mr-3 h-12"
+                            className="mr-2 h-10"
                             alt="Logo"
                         />
                     </Link>
